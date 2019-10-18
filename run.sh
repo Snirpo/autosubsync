b0341ddc02406cba83f8f19ac62836ba5dc0ec70
snirpo <snirpo@gmail.com>
2019-10-18 21:57:36 +0200

    updated dependencies to latest major version

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
-set GOOGLE_APPLICATION_CREDENTIALS=config/speech_auth.json
+#!/bin/bash
+export GOOGLE_APPLICATION_CREDENTIALS=config/speech_auth.json
 node dist/cli.js demo/walkingdead.mkv -d 240 -s 0.1 -t 0.8 -r 1 --log debug > output.txt
